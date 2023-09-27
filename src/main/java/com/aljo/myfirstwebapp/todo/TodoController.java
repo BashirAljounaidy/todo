f3f9af7869edbdf67ee1d5d716d659bb31ae2e40
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2023-09-27 14:19:17 +0400

    step 21 complete

--- src/main/java/com/aljo/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/aljo/myfirstwebapp/todo/TodoController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import java.time.LocalDate;
 
 @Controller
 @SessionAttributes("name")
@@ -34,7 +36,8 @@ public class TodoController {
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewTodo() {
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		todoService.addTodo(model.get("name").toString(), description, LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 
--- src/main/java/com/aljo/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/aljo/myfirstwebapp/todo/TodoService.java
@@ -10,14 +10,20 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 	
 	private static List<Todo> todos = new ArrayList<>();
+
+	private static int todoCount = 0;
 	
 	static {
-		todos.add(new Todo(1,"aljo","Learn AWS", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(2,"aljo","Learn DEVOPS", LocalDate.now().plusYears(2),false));
-        todos.add(new Todo(3,"aljo","Learn FULLSTACK", LocalDate.now().plusYears(3),false));
+		todos.add(new Todo(++todoCount,"aljo","Learn AWS", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todoCount,"aljo","Learn DEVOPS", LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(++todoCount,"aljo","Learn FULLSTACK", LocalDate.now().plusYears(3),false));
     }
     public List<Todo> findByUsername(String username){
 		return todos;
+	}
+	public void addTodo(String name, String description , LocalDate targetDate,boolean done) {
+		Todo todo = new Todo(++todoCount,name,description,targetDate,done);
+		todos.add(todo);
 	}
 	
 }
